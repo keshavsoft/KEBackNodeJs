b09cc87c82d6f8004af9d0f6e4bb5e17da9b83df
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-04-28 00:22:20 +0530

    FileAsObjectContent-Delete changed

--- src/V6/NodeJs/FileAsObjectContent/Delete/CopyCode/dals/entryFile.js
+++ src/V6/NodeJs/FileAsObjectContent/Delete/CopyCode/dals/entryFile.js
@@ -1,7 +1,7 @@
-import { StartFunc as StartFuncFromSelectedColumns } from '../kLowDb/readFromFile.js';
+import { StartFunc as StartFuncFromDeleteFromFile } from '../kLowDb/deleteFromFile.js';
 
 let postDefaultFunc = ({ inKey, inFileName }) => {
-    let LocalFromLowDb = StartFuncFromSelectedColumns({ inKey, inFileName });
+    let LocalFromLowDb = StartFuncFromDeleteFromFile({ inKey, inFileName });
 
     if (LocalFromLowDb.KTF === false) {
         return LocalFromLowDb;
