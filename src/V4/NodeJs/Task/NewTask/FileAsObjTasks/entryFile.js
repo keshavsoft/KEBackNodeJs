34f4788618f94bf9f6369cbbf94f5ba387244e57
keshavsoft <nknnkumar@yahoo.com>
2025-05-09 11:45:32 +0530

    deleted unwanted old folders

--- src/V4/NodeJs/Task/NewTask/FileAsObjTasks/entryFile.js
+++
@@ -1,9 +0,0 @@
-const { StartFunc: StartFuncFromReadFile } = require("./ReadContent/entryFile");
-const { StartFunc: StartFuncFromInsertContent } = require("./InsertContent/entryFile");
-
-const StartFunc = () => {
-    StartFuncFromReadFile();
-    StartFuncFromInsertContent();
-};
-
-module.exports = { StartFunc };
