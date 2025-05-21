40360a88a3f72d1082954b7cdd403087c647325d
Srivasu215 <sri.vasu215@gmail.com>
2025-05-21 13:04:27 +0530

    InsertWith Columns

--- src/V13/KS/BoilerPlate/FromJsonConSubRoute/AlterFiles/AlterRestFiles/entryFile.js
+++ src/V13/KS/BoilerPlate/FromJsonConSubRoute/AlterFiles/AlterRestFiles/entryFile.js
@@ -1,11 +1,17 @@
 const { StartFunc: StartFuncFromReadFolder } = require("./readFolder");
 const CommonFolderName = "Read";
+const CommonInsertFolderName = "Insert";
 
 async function StartFunc({ inEditorPath, inTableName, inPortNumber, inVersion }) {
     const LocalVersion = inVersion;
 
     await StartFuncFromReadFolder({
         inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/${CommonFolderName}/RestClients`,
+        inTableName, inPortNumber
+    });
+
+    await StartFuncFromReadFolder({
+        inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/${CommonInsertFolderName}/RestClients`,
         inTableName, inPortNumber
     });
 };
