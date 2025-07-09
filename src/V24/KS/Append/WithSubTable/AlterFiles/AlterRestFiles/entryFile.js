749baa1a5c9a359f46746b5e73d925add45a07db
Srivasu215 <sri.vasu215@gmail.com>
2025-07-09 15:54:32 +0530

    Auto gen  restcliets

--- src/V24/KS/Append/WithSubTable/AlterFiles/AlterRestFiles/entryFile.js
+++ src/V24/KS/Append/WithSubTable/AlterFiles/AlterRestFiles/entryFile.js
@@ -1,6 +1,8 @@
 const { StartFunc: StartFuncFromReadFolder } = require("./readFolder");
 const { StartFunc: StartFuncFromInsertRestFile } = require("./insertRestFile");
 const { StartFunc: StartFuncFromReadRestFile } = require("./ReadRestFile");
+const { StartFunc: StartFuncFromAlterRestFile } = require("./AlterRestFile");
+const { StartFunc: StartFuncFromDeleteRestFile } = require("./DeleteRestFile");
 
 const CommonFolderName = "Read";
 const CommonDeleteFolderName = "Delete";
@@ -31,15 +33,28 @@ async function StartFunc({ inEditorPath, inTableName, inPortNumber, inVersion, i
         inColumnsAsArray
     });
 
-    await StartFuncFromReadFolder({
-        inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/${CommonDeleteFolderName}/RestClients`,
-        inTableName, inPortNumber
+    await StartFuncFromAlterRestFile({
+        inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/Alter`,
+        inTableName, inPortNumber,
+        inColumnsAsArray
     });
 
-    await StartFuncFromReadFolder({
-        inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/${CommonAlterFolderName}/RestClients`,
-        inTableName, inPortNumber
+    await StartFuncFromDeleteRestFile({
+        inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/Delete`,
+        inTableName, inPortNumber,
+        inColumnsAsArray
     });
+
+    // await StartFuncFromReadFolder({
+    //     inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/${CommonDeleteFolderName}/RestClients`,
+    //     inTableName, inPortNumber
+    // });
+    
+
+    // await StartFuncFromReadFolder({
+    //     inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/${CommonAlterFolderName}/RestClients`,
+    //     inTableName, inPortNumber
+    // });
 
     await StartFuncFromReadFolder({
         inFolderPath: `${inEditorPath}/${LocalVersion}/${inTableName}/${CommonReadSchemaFolderName}/RestClients`,
