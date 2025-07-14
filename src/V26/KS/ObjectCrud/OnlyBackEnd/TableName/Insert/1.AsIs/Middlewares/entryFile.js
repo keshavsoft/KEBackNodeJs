98729515b03240c0bfc0dac6a69372c74c30ea8b
sharath0036 <ksharathbabu2@gmail.com>
2025-07-14 11:35:05 +0530

    objetc insert

--- src/V26/KS/ObjectCrud/OnlyBackEnd/TableName/Insert/1.AsIs/Middlewares/entryFile.js
+++ src/V26/KS/ObjectCrud/OnlyBackEnd/TableName/Insert/1.AsIs/Middlewares/entryFile.js
@@ -1,12 +1,12 @@
 const StartFunc = (req, res, next) => {
     const LocalBody = req.body;
 
-    if (!LocalBody || Object.keys(LocalBody).length === 0) {
-        return res.status(400).send("Request body should not be empty.");
+    if (!LocalBody) {
+        return res.status(400).send("Request body is required.");
     }
 
     if (Array.isArray(LocalBody)) {
-        return res.status(400).send("Remove The Array From The Body.");
+        return res.status(400).send("Request body should not be an array.");
     }
 
     if (typeof LocalBody === 'object' && Object.keys(LocalBody).length === 0) {
